bee50f6faf8acf235a81e0cad632bc8180939ea6
Victor Andrade <victor64gaming@gmail.com>
2024-10-22 21:20:25 -0300

    Adding hashCode and equals methods

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -17,6 +19,19 @@ class User {
     public User(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 
     @Override
