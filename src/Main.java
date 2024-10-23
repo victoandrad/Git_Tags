60dd1b5ca16dd84ebbd4a9fd9a03d69fed4d4aee
Victor Andrade <victor64gaming@gmail.com>
2024-10-22 21:16:20 -0300

    User class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        User user = new User("Victor Andrade", 19);
+        System.out.println(user);
+
+    }
+}
+
+class User {
+    private String name;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", age=" + age + "]";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
